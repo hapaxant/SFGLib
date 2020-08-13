9a0fa716667556e2838ff3f685928b8a31246e12
kubapolish <xkubapr0j3ctx@gmail.com>
2020-08-13 22:56:07 +0200

    add projects, references

---
+++ SFGLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using Newtonsoft.Json;
+
+namespace SFGLib
+{
+    public class Class1
+    {
+    }
+}
